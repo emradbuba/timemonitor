e0ac0e9a36b4e8bb60131536db4728b7efb13b4f
Radek Em <3488909-emradbuba@users.noreply.gitlab.com>
2025-04-07 20:27:32 +0200

    Init repository

---
+++ src/main/java/com/emradbuba/timemonitor/TimemonitorApplication.java
@@ -0,0 +1,13 @@
+package com.emradbuba.timemonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimemonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimemonitorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/emradbuba/timemonitor/TimemonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emradbuba.timemonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimemonitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
